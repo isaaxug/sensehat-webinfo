4256da951c309bd005b69b372096e87a99a9e586
Nanami Takayama <ns.potable@gmail.com>
2020-02-10 15:00:50 +0900

    third commit
    added update_ambient.py compatible with sensehat

---
+++ update_ambient.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env pyhton3
+import ambient
+import time
+import datetime
+import re
+from sense_hat import SenseHat
+
+AMBIENT_CHANNEL_ID = "18768"
+AMBIENT_WRITE_KEY = "98871282c62dd0fe"
+
+CHECK_SPAN = 30
+sense = SenseHat()
+
+if __name__ == '__main__':
+    # create Ambient Object
+    am = ambient.Ambient(AMBIENT_CHANNEL_ID, AMBIENT_WRITE_KEY)
+
+    # main loop
+    while True:
+        accs = sense.get_accelerometer()
+        # get acceleromener values
+        acc_values = [round(x,2) for x in accs.values()]
+
+        # create send data
+        data = {
+            'created': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            'd1': round(sense.get_temperature(), 1),
+            'd2': round(sense.get_humidity(), 1),
+            'd3': round(sense.get_pressure(), 1),
+            'd4': sense.get_compass(),
+            'd5': acc_values[0],
+            'd6': acc_values[1],
+            'd7': acc_values[2]}
+        # output data on Raspberry Pi
+        print(data)
+        # send data
+        am.send(data)
+        # wait time to next send timing
+        time.sleep(CHECK_SPAN)
